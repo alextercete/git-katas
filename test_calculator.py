9d8558df0cd80e9a1d6db4634cb4af8126bbd240
Alex Tercete <alex.tercete@savanta.com>
2024-04-25 21:28:50 +0100

    Replace unittest with pytest

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,9 @@
-import unittest
 from calculator import Calculator
 
-class CalculatorTests(unittest.TestCase):
-    def test_sum(self):
-        calculator = Calculator()
-        self.assertEqual(3, calculator.sum(1, 2))
+def test_sum():
+    calculator = Calculator()
+    assert calculator.sum(1, 2) == 3
 
-    def test_subtraction(self):
-        calculator = Calculator()
-        self.assertEqual(3, calculator.subtract(5, 2))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_subtraction():
+    calculator = Calculator()
+    assert calculator.subtract(5, 2) == 3
